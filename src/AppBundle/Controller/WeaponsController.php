f7c5d894778e0377460783fbc40f34f2b9658ce4
somecallmetim <timbauer@ymail.com>
2016-12-24 23:44:58 -0800

    basic weapon CRUD implemented. Still need to implement dice determiner logic so you can't end up with both a dynamic and static value. Need to add prePersist listener to make sure this isn't violated. Need to add asserts/validation to WeaponsEntity.

--- src/AppBundle/Controller/WeaponsController.php
+++ src/AppBundle/Controller/WeaponsController.php
@@ -9,15 +9,102 @@
 namespace AppBundle\Controller;
 
 
+use AppBundle\Entity\WeaponsEntity;
+use AppBundle\Form\AddWeaponForm;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Form\Form;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * @Security("is_granted('ROLE_ADMIN')")
+ */
 
 class WeaponsController extends Controller
 {
-    public function addWeaponAction(){
-
+    /**
+     * @Route("/weapon/show/{weaponName}", name="weapon_show")
+     */
+    public function showWeaponAction(WeaponsEntity $weapon){
+        return $this->render('weapons/showWeapon.html.twig', [
+            'weapon' => $weapon
+        ]);
     }
 
-    public function editWeaponAction(){
+    /**
+     * @Route("/weapon/list", name="weapon_list")
+     */
+    public function listWeaponAction(){
+        $em = $this->getDoctrine()->getManager();
 
+        $weapons = $em->getRepository('AppBundle:WeaponsEntity')
+            ->findAll();
+
+        return $this->render('weapons/listWeapons.html.twig', [
+            'weapons' => $weapons
+        ]);
+    }
+
+    /**
+     * @Route("/weapon/add", name="weapon_add")
+     */
+    public function addWeaponAction(Request $request){
+        $form = $this->createForm(AddWeaponForm::class);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+            $this->persistWeapon($form);
+
+            return $this->redirectToRoute('weapon_list');
+        }
+
+        return $this->render('weapons/addWeapon.html.twig', [
+            'form' => $form->createView()
+            ]);
+    }
+
+    /**
+     * @Route("/weapon/edit/{weaponName}", name="weapon_edit")
+     */
+    public function editWeaponAction(Request $request, WeaponsEntity $weapon){
+        $form = $this->createForm(AddWeaponForm::class, $weapon);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+            $this->persistWeapon($form);
+
+            return $this->redirectToRoute('weapon_list');
+        }
+
+        return $this->render('weapons/editWeapon.html.twig', [
+           'form' => $form->createView(),
+            'weapon' => $weapon
+        ]);
+    }
+
+    /**
+     * @Route("/weapon/remove/{id}", name="weapon_remove")
+     */
+    public function removeWeaponAction($id){
+        $em = $this->getDoctrine()->getManager();
+
+        $weapon = $em->getRepository('AppBundle:WeaponsEntity')->find($id);
+
+        $em->remove($weapon);
+        $em->flush();
+
+        return $this->redirectToRoute('weapon_list');
+    }
+
+    private function persistWeapon(Form $form){
+        $em = $this->getDoctrine()->getManager();
+        $weapon = $form->getData();
+
+        $em->persist($weapon);
+        $em->flush();
     }
 }
---
+++ src/AppBundle/Form/AddWeaponForm.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace AppBundle\Form;
+
+use AppBundle\Entity\WeaponsEntity;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class AddWeaponForm extends AbstractType
+{
+
+    private $choiceValues = [
+        'placeholder' => 'Please Select A Value',
+        'choices' => [
+            '0' => 0,
+            '1' => 1,
+            '2' => 2,
+            '3' => 3,
+            '4' => 4,
+            '5' => 5,
+            '6' => 6,
+            '7' => 7,
+            '8' => 8,
+            '9' => 9,
+            '*' => -1
+        ]
+    ];
+
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('weaponName')
+            ->add('minRange')
+            ->add('maxRange')
+            ->add('attacks', ChoiceType::class, $this->choiceValues)
+            ->add('toHit', ChoiceType::class, $this->choiceValues)
+            ->add('toWound', ChoiceType::class, $this->choiceValues)
+            ->add('dmgDeterminer', ChoiceType::class, [
+                'choices' => [
+                    'dice' => 'dice',
+                    'static value' => 'static'
+                ]
+            ])
+            ->add('maxDieDmgValue', ChoiceType::class, [
+                'choices' => [
+                    '3' => 3,
+                    '6' => 6
+                ]
+            ])
+            ->add('numberOfDmgDice')
+            ->add('staticDmg')
+            ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => 'AppBundle\Entity\WeaponsEntity'
+        ]);
+    }
+
+}
